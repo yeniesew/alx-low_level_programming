06434787fe793225fdf60a194f22a95871f177b9
yeniesew <yebrielij@gmail.com>
2022-12-12 02:49:49 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - prints all single digit numbers
+ * Return: 0
+ */
+int main(void)
+{
+int c;
+c = 0;
+while (c < 10)
+{
+putchar(c + '0');
+c++;
+}
+putchar('\n');
+return (0);
+}
