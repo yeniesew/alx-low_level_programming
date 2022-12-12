5cea93fbaf1c1b072b7f9cb294bcabb4c336143d
yeniesew <yebrielij@gmail.com>
2022-12-12 00:59:39 -0800

    alphABET

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - prints the alphabetin lowercase
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+char c;
+c = 'a';
+while (c <= 'z')
+{
+putchar('\n');
+c++;
+}
+return (0);
+}
