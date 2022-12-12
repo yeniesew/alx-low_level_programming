a0e11c742bee7cedfc1bf60b94b00edc878d4f8b
yeniesew <yebrielij@gmail.com>
2022-12-12 03:09:07 -0800

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ * Return: 0
+ */
+int main(void)
+{
+char c;
+int d;
+c = 'a';
+d = 0;
+while (d < 10)
+{
+putchar(d + '0');
+d++;
+}
+while (while c <= 'f')
+{
+putchar(c);
+c++;
+}
+put char('\n');
+return (0);
+}
+
+
