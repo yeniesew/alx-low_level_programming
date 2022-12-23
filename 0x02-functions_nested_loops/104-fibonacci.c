1e1bcbd93116f279b5aa5826b51b9254ba428560
yeniesew <yebrielij@gmail.com>
2022-12-22 23:32:35 -0800

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -31,7 +31,7 @@ printf(", %lu", aft1 + (aft2 / l));
 printf("%lu", aft2 % l);
 aft1 = aft1 + bef1;
 bef1 = aft1 - bef1;
-ft2 = aft2 + bef2;
+aft2 = aft2 + bef2;
 bef2 = aft2 - bef2;
 }
 printf("\n");
