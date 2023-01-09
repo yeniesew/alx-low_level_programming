529c8450ce94af2c4e5afa010df44505bb25c9c3
yeniesew <yebrielij@gmail.com>
2023-01-09 01:28:44 -0800

    advanced task 2

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -51,7 +51,7 @@ tmp = (char *) malloc(sizeof(char) * (c + 1));
 if (tmp == NULL)
 return (NULL);
 while (start < end)
-*mp++ = str[start++];
+*tmp++ = str[start++];
 *tmp = '\0';
 matrix[k] = tmp - c;
 k++;
