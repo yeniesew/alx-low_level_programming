9acf2d3d400810af8640e27d16c86afd85474508
yeniesew <yebrielij@gmail.com>
2023-01-09 05:45:21 -0800

    6

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -4,7 +4,7 @@
 * *count_word - helper function to count the number of words in a string
 * *@s: string to evaluate
 * *Return: number of words
-* */
+*/
 int count_word(char *s)
 {
 int flag, c, w;
