f9c6052c0e6725ed89517e73723524d6f074a1ba
yeniesew <yebrielij@gmail.com>
2023-01-09 03:49:40 -0800

    last

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -23,7 +23,7 @@ w++;
 return (w);
 }
 /**
-* **strtow - splits a string into words
+* strtow - splits a string into words
 * @str: string to split
 * Return: pointer to an array of strings (Success)
 * or NULL (Error)
