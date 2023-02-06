d374fd3b4c614fff09203703c7a285c42d25ccd2
yeniesew <yebrielij@gmail.com>
2023-02-06 01:36:21 -0800

    advanced

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -209,7 +209,7 @@ printf("  Entry point address:               ");
 if (e_ident[EI_DATA] == ELFDATA2MSB)
 {
 e_entry = ((e_entry << 8) & 0xFF00FF00) |
- ((e_entry >> 8) & 0xFF00FF);
+((e_entry >> 8) & 0xFF00FF);
 e_entry = (e_entry << 16) | (e_entry >> 16);
 }
 if (e_ident[EI_CLASS] == ELFCLASS32)
