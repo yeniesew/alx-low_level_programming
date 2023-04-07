88d40ebde1ca186fa27c069afdcead180c645d4d
yeniesew <yebrielij@gmail.com>
2023-04-07 00:10:22 -0700

    1-djb2.c

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -10,6 +10,7 @@ unsigned long int hash;
 int c;
 hash = 5381;
 while ((c = *str++))
-hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+hash = ((hash << 5) + hash) +c;
+/* hash * 33 + c */
 return (hash);
 }
